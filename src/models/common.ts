6b2ecbb9cf5acca6a43a077771db4b9d71b3598c
Alberto Elorza <albertoelorza2002@gmail.com>
2022-09-13 16:42:51 +0200

    common models

---
+++ src/models/common.ts
@@ -0,0 +1,58 @@
+import { Collection, Document, MatchKeysAndValues } from "mongodb";
+
+export abstract class Model<T extends Document> {
+    constructor(protected collection: Collection<T>) {
+
+    }
+
+    async find(query?: Partial<T>): Promise<T | null> {
+        return await this.collection.findOne(query || {}) as T | null;
+    }
+
+    async findAll(query?: Partial<T>): Promise<T[]> {
+        return await this.collection.find(query || {}).toArray() as T[];
+    }
+
+    async insert(data: T): Promise<void> {
+        await this.collection.insertOne(data as any);
+    }
+
+    async insertMany(data: T[]): Promise<void> {
+        await this.collection.insertMany(data as any[]);
+    }
+
+    async update(query: Partial<T>, changes: MatchKeysAndValues<T>) {
+        await this.collection.updateOne(query, {"$set": changes})
+    }
+    
+    async updateAll(query: Partial<T>, changes: MatchKeysAndValues<T>) {
+        await this.collection.updateMany(query, {"$set": changes})
+    }
+}
+
+export abstract class SavableModel<T extends Document, K> extends Model<T> {
+    abstract convert(item: K): T;
+
+    async save(item: K): Promise<void> {
+        await this.insert(this.convert(item) as any);
+    }
+
+    async saveMany(items: K[]): Promise<void> {
+        await this.insertMany(items.map(this.convert.bind(this)) as any[]);
+    }
+
+    async saveOnce(item: K, ref: keyof T): Promise<void> {
+        const s = this.convert(item);
+        const query: Partial<T> = {};
+        query[ref] = s[ref];
+        if (!await this.find(query)) this.insert(s);
+    }
+
+    async saveOrUpdate(item: K, ref: keyof T): Promise<void> {
+        const s = this.convert(item);
+        const query: Partial<T> = {};
+        query[ref] = s[ref];
+        if (!await this.find(query)) this.insert(s);
+        else this.update(query, s);
+    }
+}
